61469dae9da40f3d13a2d818ba4f3a1eadcc1db2
Amber Feng <amber.feng@gmail.com>
2009-11-16 21:11:35 +0000

    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@17 3207efe2-bddd-11de-9616-7b1d4728551e

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -9,7 +9,19 @@ class JobsController < ApplicationController
       format.xml  { render :xml => @jobs }
     end
   end
-
+  
+  def list
+	d_id = params[:department_select]
+	if(d_id == "0")
+		@department = "All Departments"
+		@jobs = Job.all
+	else
+		@department = Department.find(d_id).name
+		@jobs = Job.all
+	end
+	
+  end
+  
   ***REMOVED*** GET /jobs/1
   ***REMOVED*** GET /jobs/1.xml
   def show
---
+++ test/unit/department_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class DepartmentTest < ActiveSupport::TestCase
+  ***REMOVED*** Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
