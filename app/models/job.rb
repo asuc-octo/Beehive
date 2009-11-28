bd0b444bc86b7c568cfbaf940f67ce152099a16c
Oren Blasberg <oblasberg@gmail.com>
2009-11-28 06:44:54 +0000

    Almost done with this:  Jobs now have editable categories with working autocomplete.  However, need to grab existing value of category.. next rev should cover that
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@113 3207efe2-bddd-11de-9616-7b1d4728551e

--- app/models/job.rb
+++ app/models/job.rb
@@ -7,12 +7,33 @@ class Job < ActiveRecord::Base
   has_many :sponsorships
   has_many :faculties, :through => :sponsorships
   
+  ***REMOVED*** Before carrying out validations and creating the actual object, 
+  ***REMOVED*** handle the name of the category(ies) so as to deal with associations
+  ***REMOVED*** properly.
+  before_validation :handle_categories
+  
   acts_as_solr :fields => [:title, :desc]
   
   validates_presence_of :title, :desc, :exp_date, :num_positions, :department
   
+  attr_accessor :category_names
+  
   def self.find_recently_added(n)
 	Job.find(:all, :order => "created_at DESC", :limit=>n)
   end
+  
+  
+  protected
+  
+  	***REMOVED*** Parses the textbox list of category names from "Signal Processing, Robotics"
+	***REMOVED*** etc. to an enumerable object categories
+	def handle_categories
+		self.categories = []  ***REMOVED*** eliminates any previous categories_jobs so as to avoid duplicates
+		category_array = []
+		category_array = category_names.split(',').uniq if ! category_names.nil?
+		category_array.each do |item|
+			self.categories << Category.find_or_create_by_name(item.downcase.strip)
+		end
+	end
   
 end
