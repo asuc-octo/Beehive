0f46ab1486ed1fc3bea5e415296310474ed3e8d3
Oren Blasberg <oblasberg@gmail.com>
2009-11-29 00:25:16 +0000

    Fixed stupid "<<<<<<<" stuff caused by bad conflict resolution
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@125 3207efe2-bddd-11de-9616-7b1d4728551e

--- vendor/plugins/acts_as_taggable_on_steroids/lib/acts_as_taggable.rb
+++ vendor/plugins/acts_as_taggable_on_steroids/lib/acts_as_taggable.rb
@@ -1,4 +1,3 @@
-<<<<<<< .mine
 module ActiveRecord ***REMOVED***:nodoc:
   module Acts ***REMOVED***:nodoc:
     module Taggable ***REMOVED***:nodoc:
@@ -229,22 +228,3 @@ module ActiveRecord ***REMOVED***:nodoc:
 end
 
 ActiveRecord::Base.send(:include, ActiveRecord::Acts::Taggable)
-=======
-module ActiveRecord ***REMOVED***:nodoc:
-  module Acts ***REMOVED***:nodoc:
-    module Taggable ***REMOVED***:nodoc:
-      def self.included(base)
-        base.extend(ClassMethods)
-      end
-      
-      module ClassMethods
-        def acts_as_taggable
-          raise "acts_as_taggable_on_steroids has been moved to github: http://github.com/jviney/acts_as_taggable_on_steroids"
-        end
-      end
-    end
-  end
-end
-
-ActiveRecord::Base.send(:include, ActiveRecord::Acts::Taggable)
->>>>>>> .r121
