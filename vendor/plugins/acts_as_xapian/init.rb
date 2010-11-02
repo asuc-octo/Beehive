b1075dd4375b24adff46df241bf10a2929041a61
Jonathan Ko <jonathan.ko@berkeley.edu>
2010-11-02 07:38:12 +0000

    removing acts_as_xapian
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@310 3207efe2-bddd-11de-9616-7b1d4728551e

--- vendor/plugins/acts_as_xapian/init.rb
+++
@@ -1,9 +0,0 @@
-***REMOVED*** acts_as_xapian/init.rb:
-***REMOVED***
-***REMOVED*** Copyright (c) 2008 UK Citizens Online Democracy. All rights reserved.
-***REMOVED*** Email: francis@mysociety.org; WWW: http://www.mysociety.org/
-***REMOVED***
-***REMOVED*** $Id: init.rb,v 1.1 2008/04/23 13:33:50 francis Exp $
-
-require 'acts_as_xapian'
-
