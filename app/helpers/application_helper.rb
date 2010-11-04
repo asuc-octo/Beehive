a45f73a5b0c6dc582aed1b25c39883bf53f120f7
Oren Blasberg <oblasberg@gmail.com>
2010-11-04 02:54:12 +0000

    Tag cloud
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@315 3207efe2-bddd-11de-9616-7b1d4728551e

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 ***REMOVED*** Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+	include TagsHelper
   
 	module QueryHelpers
 		def as_OR_query
