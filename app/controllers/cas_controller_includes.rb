a10158986ab6b54bc476f73a312d9d1406cdd9d6
Oren Blasberg <oblasberg@gmail.com>
2011-01-01 23:32:50 +0000

    Forgot to commit a file
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@386 3207efe2-bddd-11de-9616-7b1d4728551e

---
+++ app/controllers/cas_controller_includes.rb
@@ -0,0 +1,12 @@
+module CASControllerIncludes
+
+  ***REMOVED*** this before_filter takes the results of the rubyCAS-client filter and sets up the current_user, 
+  ***REMOVED*** thereby "logging you in" as the proper user in the ResearchMatch database.
+  def setup_cas_user
+    return unless session[:cas_user].present?
+    @current_user = User.find_by_login(session[:cas_user])
+    @current_user.present?
+  end
+
+end
+
