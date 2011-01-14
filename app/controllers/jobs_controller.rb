1d5e3e7a2d3cbcdc5976146cba86a4992c47b8c4
Oren Blasberg <oblasberg@gmail.com>
2011-01-14 01:34:46 +0000

    We need to prevent unauthenticated users from accessing jobs.
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@422 3207efe2-bddd-11de-9616-7b1d4728551e

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -15,7 +15,7 @@ class JobsController < ApplicationController
   ***REMOVED***before_filter :goto_cas_unless_logged_in
     
   ***REMOVED*** Ensures that only logged-in users can create, edit, or delete jobs
-  before_filter :rm_login_required, :except => [ :index, :show ]
+  before_filter :rm_login_required
   
   ***REMOVED*** Ensures that only the user who created a job -- and no other users -- can edit 
   ***REMOVED*** or destroy it.
