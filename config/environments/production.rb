84bd07803188a88d9e29adb141abf1b7dbbadf8d
Oren Blasberg <oblasberg@gmail.com>
2011-01-14 02:31:48 +0000

    ActionMailer enabled for PRODUCTION ONLY (hopefully)
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@425 3207efe2-bddd-11de-9616-7b1d4728551e

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,9 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
+***REMOVED*** ActionMailer should actually send mail when using the production server
+ActionMailer::Base.delivery_method = :smtp
+
 ***REMOVED*** See everything in the log (default is :info)
 ***REMOVED*** config.log_level = :debug
 
@@ -26,3 +29,4 @@ config.action_view.cache_template_loading            = true
 
 ***REMOVED*** Enable threaded mode
 ***REMOVED*** config.threadsafe!
+
