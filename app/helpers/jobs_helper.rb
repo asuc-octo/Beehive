aaf58ddecc8b5e6e411bba34e324af250eba7bac
Oren Blasberg <oblasberg@gmail.com>
2011-01-15 00:40:20 +0000

    TEMP FIX: Temporarily changing the url generated by activation_url method because it was generating wacky http://http:// ... URLs before.
    git-svn-id: https://research-cs194.googlecode.com/svn/trunk/research@431 3207efe2-bddd-11de-9616-7b1d4728551e

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,6 +1,6 @@
 module JobsHelper
   def activation_url(job)
-    ***REMOVED***"***REMOVED***{$rm_root}jobs/activate/***REMOVED***{self.id}?a=***REMOVED***{self.activation_code}"
-    "***REMOVED***{activate_job_url(job)}?a=***REMOVED***{job.activation_code}"
+    "***REMOVED***{$rm_root}jobs/activate/***REMOVED***{self.id}?a=***REMOVED***{job.activation_code}"
+    ***REMOVED***"***REMOVED***{activate_job_url(job)}?a=***REMOVED***{job.activation_code}"
   end
 end
