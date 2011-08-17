77119d1db5d00db97f770a2857fd7395cccfd6ae
Oren Blasberg <oblasberg@gmail.com>
2011-08-16 22:24:46 -0700

    Fixed activation-related bugs.
    Fixed:
    - Could view show page for job that is not yet activated.
    - Could see unactivated jobs in searches.
    - Bug where activation email would not reset the activation code properly.
    
    Still not yet fixed:
    - Can APPLY to unactivated job.

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,5 +1,6 @@
 module JobsHelper
   def activation_url(job)
+    puts "\n\n\nACTIVATION COOOOOODE" + job.activation_code.to_s + "\n\n\n\n"
     "***REMOVED***{ROOT_URL}***REMOVED***{activate_job_path(job)}?a=***REMOVED***{job.activation_code}"
   end
 end
