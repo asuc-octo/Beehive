30de87c1179924e5e66cdb182ae5012e9ff3c4a4
Jonathan Ko <jonathan.ko@berkeley.edu>
2011-11-08 03:50:39 -0800

    Admin box on jobs#show
    For now, just shows activation url
    Also added logged_in_as_admin? helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,6 +21,12 @@ module ApplicationHelper
     ActiveSupport::Deprecation.warn "logged_in? is deprecated in favor of @current_user", caller
     !!@current_user
   end
+
+  ***REMOVED*** Checks if user is logged in as an admin.
+  ***REMOVED*** @return [Boolean] true if {***REMOVED***current_user} is set and is {User***REMOVED***admin?}
+  def logged_in_as_admin?
+    @current_user and @current_user.admin?
+  end
 end
 
 module ActionView
