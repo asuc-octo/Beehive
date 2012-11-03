724d130e435a2210c3cef61888662027f1d6329a
Jeff Tsui <tsui.jeff@gmail.com>
2012-11-03 15:45:03 -0700

    listing status user story done. todo cuc tests

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -10,3 +10,10 @@ When /^I log out$/ do
   end
   visit "/"
 end
+
+Then /^I should see "(.)" after "(.)"$/ do |e1, e2|
+  ***REMOVED***  ensure that that e1 occurs before e2.
+  ***REMOVED***  page.content  is the entire content of the page as a string.
+  regexp = /***REMOVED***{e2}.****REMOVED***{e1}/m
+  assert_match regexp, page.body
+end
