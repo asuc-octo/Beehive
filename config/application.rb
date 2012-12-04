cfa354e975d476239158abd3bf9259ad4dbe0c68
saasbook <saasbook@saasbook.(none)>
2012-12-03 18:41:09 -0800

    Fixing js

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,9 @@ module ResearchMatch
     ***REMOVED*** config.i18n.default_locale = :de
 
     ***REMOVED*** JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(jquery jquery_ujs jquery-ui-1.8.4.custom.min autocomplete_setup)
+    ***REMOVED*** config.action_view.javascript_expansions[:defaults] = %w(jquery jquery_ujs jquery-ui-1.8.4.custom.min autocomplete_setup)
+    config.action_view.javascript_expansions[:defaults] = %w(jquery-1.8.3 jquery_ujs jquery-ui-1.9.2 autocomplete_setup)
+
 
     ***REMOVED*** Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
