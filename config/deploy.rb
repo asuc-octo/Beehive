7c5e46f7dc19513213914c8a686edc421dd8d9d8
Max Lam <agnusmaximus@gmail.com>
2014-03-06 23:27:25 -0800

    Changed all occurrences of researchmatch -> BeeHive

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 require "***REMOVED***{File.dirname(__FILE__)}/capistrano_database"
 
-set :application, "researchmatch"
+set :application, "BeeHive"
 set :repository,  "git://github.com/jonathank/ResearchMatch.git"
 set :scm, "git"
 ***REMOVED*** Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
@@ -10,7 +10,7 @@ set :branch, "rails3"
 set :machine_name, "upe.cs.berkeley.edu"
 
 ***REMOVED*** Directory for deployment on the production (remote) machine.
-set :deploy_to, "/home/amber/researchmatch/"
+set :deploy_to, "/home/amber/BeeHive/"
 
 role :web, "***REMOVED***{machine_name}"                          ***REMOVED*** Your HTTP server, Apache/etc
 role :app, "***REMOVED***{machine_name}"                          ***REMOVED*** This may be the same as your `Web` server
