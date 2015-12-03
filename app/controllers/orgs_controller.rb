707f7e1884395929ac7fca3069c68c3232016095
Kevin Ma <kevinma.sd@gmail.com>
2015-12-02 17:34:48 -0800

    Remove redundant before_filter

--- app/controllers/orgs_controller.rb
+++ app/controllers/orgs_controller.rb
@@ -1,7 +1,6 @@
 class OrgsController < ApplicationController
 
   ***REMOVED*** Only logged in users can view this page
-***REMOVED***  before_filter :goto_home_unless_logged_in
   before_filter :rm_login_required
 
   ***REMOVED*** Only users in the org can modify it
