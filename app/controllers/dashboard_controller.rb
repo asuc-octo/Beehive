420158b64f397834084f06e3bf42f6a2ab62a93e
LEON-MING <leonming12@yahoo.com>
2018-06-06 17:30:55 -0700

    added test analytics event

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,6 @@ class DashboardController < ApplicationController
   before_filter :rm_login_required
 
   def index
-
+      Analytics.track(user_id: @current_user.id, event: "Dashboard Viewed")
   end
 end
