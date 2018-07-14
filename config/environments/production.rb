f8a4ad3de19200a8ec33def0510f6e6861c31466
LEON-MING <leonming12@yahoo.com>
2018-07-14 15:54:17 -0700

    updated segment write key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ ResearchMatch::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  # config.serve_static_assets = false
 
   # compression
   # sass-rails compresses CSS by default
--- config/initializers/analytics_ruby.rb
+++ config/initializers/analytics_ruby.rb
@@ -1,4 +1,4 @@
 Analytics = Segment::Analytics.new({
-    write_key: '29YrYgRuROtuV4Ub6kNLEUWl5cT7HCHI',
+    write_key: 'QYjCRRJrSmgq0M2irqd4r7IvMqvfVuyM',
     on_error: Proc.new { |status, msg| print msg }
 })
