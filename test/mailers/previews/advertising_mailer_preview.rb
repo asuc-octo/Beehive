1789562554c7111249d79a4ad5b539f5165d5826
LEON-MING <leonming12@yahoo.com>
2019-04-12 14:08:16 -0700

    added mailer previews

--- test/mailers/previews/advertising_mailer_preview.rb
+++ test/mailers/previews/advertising_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/advertising_mailer
 class AdvertisingMailerPreview < ActionMailer::Preview
-
+  def advertising_email
+    AdvertisingMailer.advertising_email("body here", "Hello", User.find_by(email: "leon.ming@berkeley.edu"))
+  end
 end
