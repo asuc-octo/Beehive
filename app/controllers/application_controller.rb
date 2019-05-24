597b8ebd23e747ab842d3b0199c6ec842f5c640f
LEON-MING <leonming12@yahoo.com>
2019-05-23 23:45:07 -0700

    temp test commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -69,6 +69,7 @@ class ApplicationController < ActionController::Base
       flash[:notice] = "Signed in automatically as Beehive Team"
     end
     @user_session = UserSession.find
+    puts "Found a user_session"
     @current_user = @user_session.user if @user_session
   end
 
