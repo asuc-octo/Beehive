5114a4e63436e17409624770d1faa704f5087cb5
Leon Ming <leon.ming@berkeley.edu>
2019-12-13 13:56:38 -0800

    don't load Analytics for asset precompilation

--- config/initializers/analytics_ruby.rb
+++ config/initializers/analytics_ruby.rb
@@ -1,4 +1,6 @@
-Analytics = Segment::Analytics.new({
-    write_key: ENV["ANALYTICS_ID"],
-    on_error: Proc.new { |status, msg| print msg }
-})
+unless ENV['ANALYTICS_ID'].nil?
+    Analytics = Segment::Analytics.new({
+        write_key: ENV['ANALYTICS_ID'],
+        on_error: Proc.new { |status, msg| print msg }
+    })
+end
